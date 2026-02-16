58b0442146905dd93ff5fbc6ff8a721aa6f4cc0c
lkdreiling <dreilingl14@gmail.com>
2026-02-16 14:46:29 -0500

    Create run_dashboard.bat

---
+++ run_dashboard.bat
@@ -0,0 +1,11 @@
+@echo off
+:: This moves the command window to your C:\Weather folder
+cd /d F:\Weather
+
+:: This tells Windows where to find your Anaconda tools
+call %USERPROFILE%\anaconda3\Scripts\activate.bat
+
+echo Starting Irrigation Dashboard...
+streamlit run et_dashboard.py
+
+pause
